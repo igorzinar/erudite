22a11910849ef5b73d416f274268756704224919
Igor Zinar <igorzinar@gmail.com>
2020-12-29 06:59:31 +0200

    add fetch with open trivia database

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import Question from './components/Question'
+import CategorySelector from './components/CategorySelector'
+import ResultModal from './components/ResultModal'
+import Scoreboard from './components/Scoreboard'
+import './App.css'
 
-function App() {
+export default function App() {
+  const [question, setQuestion] = useState(null)
+  useEffect(() => {
+    getQuestion()
+  }, [])
+  function getQuestion() {
+    const url = 'https://opentdb.com/api.php?amount=1'
+
+    fetch(url).then((res) =>
+      res.json().then((data) => {
+        console.log(data)
+      })
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="app">
+      {/* show the result modal ----------------------- */}
+      {/* <ResultModal /> */}
 
-export default App;
+      {/* question header ----------------------- */}
+      <div className="question-header">
+        <CategorySelector />
+        <Scoreboard />
+      </div>
+
+      {/* the question itself ----------------------- */}
+      <div className="question-main">
+        <Question />
+      </div>
+
+      {/* question footer ----------------------- */}
+      <div className="question-footer">
+        <button>Go to next question 👉</button>
+      </div>
+    </div>
+  )
+}
